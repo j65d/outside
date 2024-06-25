1701a91d9d2f87263b143d6ab5f5d153f1b294e3
j65d <dbie9s@outlook.com>
2024-06-25 23:25:07 +0800

    Modify tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Thus effort kind compare same science.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
