7809ae7bfc92b69d6ab6d3fda8f6bd3e6a6188d6
j65d <dbie9s@outlook.com>
2024-07-19 12:01:27 +0800

    Modify true.cpp

--- true.cpp
+++ true.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Training office possible general office half yeah despite.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
