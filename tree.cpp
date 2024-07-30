cba5ada6f80caa36cdf0a137b436d3c72446354a
j65d <dbie9s@outlook.com>
2024-07-30 14:35:09 +0800

    Modify tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Measure use former record.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
