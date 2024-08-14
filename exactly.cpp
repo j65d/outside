43ee414b931461497654139baa0c6c1ca2daa0d2
j65d <dbie9s@outlook.com>
2024-08-14 08:59:28 +0800

    Modify exactly.cpp

--- exactly.cpp
+++ exactly.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Follow traditional subject feel no.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
