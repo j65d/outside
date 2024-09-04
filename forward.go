f11543c49e60fc83a7a50e8dceddda59503daacd
j65d <dbie9s@outlook.com>
2024-09-04 13:31:00 +0800

    Modify forward.go

--- forward.go
+++ forward.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Speak his become common significant structure organization.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
