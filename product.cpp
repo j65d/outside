78e0ffcb84cf8f1e7d93355d3d8bc94e73a80a50
j65d <dbie9s@outlook.com>
2024-09-21 10:14:41 +0800

    Modify product.cpp

--- product.cpp
+++ product.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Billion military law day discuss high.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
