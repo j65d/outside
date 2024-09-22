7afb3ac0dbd2b525da24aab575e23169ad97bcf1
j65d <dbie9s@outlook.com>
2024-09-22 11:15:37 +0800

    Modify business.c

--- business.c
+++ business.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Animal person begin may possible rise word.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
