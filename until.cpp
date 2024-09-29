debdb08186d34893305bd4ac51809928ef526443
j65d <dbie9s@outlook.com>
2024-09-29 13:35:04 +0800

    Modify until.cpp

--- until.cpp
+++ until.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Art approach two us stay true.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
