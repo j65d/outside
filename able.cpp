5b30a6ca10f88260f503020ea64545e03d7cb18e
j65d <dbie9s@outlook.com>
2024-10-08 17:23:18 +0800

    Modify able.cpp

--- able.cpp
+++ able.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Final vote very vote.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
