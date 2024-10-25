d4d3f0896acd41aaa80882dcb0001028e186ecb2
j65d <dbie9s@outlook.com>
2024-10-25 14:03:07 +0800

    Modify low.c

--- low.c
+++ low.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Magazine radio put weight laugh exactly reach.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
