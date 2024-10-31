af0073d4b065906631d3e6c022b50e33a8c160a2
j65d <dbie9s@outlook.com>
2024-10-31 13:21:25 +0800

    Modify cup.go

--- cup.go
+++ cup.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Program modern set challenge issue campaign.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
