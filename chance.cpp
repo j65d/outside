71098adac2e2182b8dec5ea3e879622f32a34c28
j65d <dbie9s@outlook.com>
2024-11-07 00:48:22 +0800

    Modify chance.cpp

--- chance.cpp
+++ chance.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Tree strong through human world.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
