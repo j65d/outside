bca62fa7a14d8751096e5032230d71f32d7149bb
j65d <dbie9s@outlook.com>
2024-11-30 23:49:33 +0800

    Modify staff.cpp

--- staff.cpp
+++ staff.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Own especially remain after consider heart.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
