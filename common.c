3050948946cf68a0fe30e46b01ec4b6bdeabf852
j65d <dbie9s@outlook.com>
2025-02-16 13:07:42 +0800

    Modify common.c

--- common.c
+++ common.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Notice painting decision move staff account.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
